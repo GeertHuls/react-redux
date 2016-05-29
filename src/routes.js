295b2a2acc0120a6c106f0c53d26801fdd6c8c5b
Geert Huls <geert.huls82@gmail.com>
2016-05-29 22:48:18 +0200

    Added the courses page

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-
+import CoursesPage from './components/course/CoursesPage';
 
 //The HomePage and AboutPage are examples of child components with are passed in,
 //into the app component.
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="courses" component={CoursesPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
