cdd11c8f1129c7c0cbb2a8296f550c60aaff0c0d
Geert Huls <geert.huls82@gmail.com>
2016-06-01 23:26:01 +0200

    Enhanced store by adding the immutable invariant middleware component

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 //initialState arg is important especially at server side rendering
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
-    initialState
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant() /*, add more middleware */)
   );
 }
