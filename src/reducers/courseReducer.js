9f8871bb02d612ad366547bc20fa7205d27b876c
Geert Huls <geert.huls82@gmail.com>
2016-06-02 23:17:27 +0200

    Created actions constants file

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,12 @@
+import * as types from '../actions/actionTypes';
+
 // Set inital state as an empty array by default.
 // If an initial state is provided with the configureStore function
 // then then that state will get passed here.
 // Use inital state with server side rendered apps.
 export default function CourseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       //Create new array (using spread operator) and add a new value.
       return[...state,
         Object.assign({}, action.course)
