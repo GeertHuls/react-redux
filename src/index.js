0418aec4d08f2aaa3956f331458161db6e43a472
Geert Huls <geert.huls82@gmail.com>
2016-06-02 21:48:52 +0200

    Connect redux store to entire app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 //Some features in ES6 cannot be transpiled by Babel, hence the polyfill.
-//Use smaller, granual polyfills instead, because the polyfill that is used 
+//Use smaller, granual polyfills instead, because the polyfill that is used
 //here is quite large (50k minified).
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+//The provider attaches a store to a react container component.
+import {Provider} from 'react-redux';
 //BrowserHistory provides cleaner urls, but are only compatible
 //with modern browser's html5 push state.
 //Hash urls are the counter part.
@@ -12,7 +15,12 @@ import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+//In case of a server side rendered, you might need an initial state.
+const store = configureStore(/*initialState*/);
+
 render(
-	<Router history={browserHistory} routes={routes} />,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</Provider>,
 	document.getElementById('app')
 );
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,3 +1,7 @@
+// Set inital state as an empty array by default.
+// If an initial state is provided with the configureStore function
+// then then that state will get passed here.
+// Use inital state with server side rendered apps.
 export default function CourseReducer(state = [], action) {
   switch (action.type) {
     case 'CREATE_COURSE':
