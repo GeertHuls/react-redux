84760137da308f4e71050250d7f21259b302daf0
Geert Huls <geert.huls82@gmail.com>
2016-06-03 23:11:39 +0200

    Created a 'load all courses' async api call as an action

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 //initialState arg is important especially at server side rendering
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant() /*, add more middleware */)
+    applyMiddleware(thunk, reduxImmutableStateInvariant() /*, add more middleware */)
   );
 }
