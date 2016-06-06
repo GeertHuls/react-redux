3f5abf0ff9b6e17c848c2f94f2eb690c07b8084e
Geert Huls <geert.huls82@gmail.com>
2016-06-06 21:30:15 +0200

    CourseFrom usage in ManageCoursePage

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,26 +3,37 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props,context);
+
+    this.state = {
+      course: Object.assign({}, props.initialCourse),
+      errors: {}
+    };
   }
 
   render() {
     return (
-      <h1>Manage course</h1>
+      <CourseForm
+        allAuthors={[]}
+        course={this.state.course}
+        errors={this.state.errors}
+        />
       );
   }
 }
 
 ManageCoursePage.propTypes = {
-  //props
+  initialCourse: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+  let course = {id:'', watchHref:'', title:'', authorId:'', length:'', category:''};
 	return {
-		state: state
+		course: course
 	};
 }
 
