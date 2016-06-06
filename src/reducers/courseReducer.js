77eb00b0b6470e3c5d02a97f6ee818f0d5850f0e
Geert Huls <geert.huls82@gmail.com>
2016-06-06 22:16:00 +0200

    AuthorReducer + created initialStateClass for store overview

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,11 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 // Set inital state as an empty array by default.
 // If an initial state is provided with the configureStore function
 // then then that state will get passed here.
 // Use inital state with server side rendered apps.
-export default function CourseReducer(state = [], action) {
+export default function CourseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
---
+++ src/reducers/initialState.js
@@ -0,0 +1,7 @@
+//This class simply provides a clear view of what
+//portions are located in the store.
+//Basically this is what the store looks like.
+export default {
+  authors: [],
+  courses: []
+};
