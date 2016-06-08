baa8eb6968abcc0374d260060263f3d51aba99fe
Geert Huls <geert.huls82@gmail.com>
2016-06-08 21:39:55 +0200

    Load course from the course route

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -58,8 +58,19 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object.isRequired
 };
 
+function getCourseById(courses, courseId) {
+  const course = courses.filter(c => c.id === courseId);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; //from the path '/course/:id'
   let course = {id:'', watchHref:'', title:'', authorId:'', length:'', category:''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
