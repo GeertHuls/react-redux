d4a7fb63c49286bd9d5d6a07de1d35b44257e307
Geert Huls <geert.huls82@gmail.com>
2016-06-11 21:24:20 +0200

    Connect the top level app component to redux to pass down loading prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <Header/>
+        <Header loading={this.props.loading}/>
         {this.props.children}
       </div>
     );
@@ -16,7 +17,17 @@ class App extends React.Component {
 //Prop type validations:
 //The 'children' prop is required, ensure it exists since it is passed in by react-router.
 App.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default App;
+//Connect the redux store to the top level app component and pass
+//the loading status down to the header.
+
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
