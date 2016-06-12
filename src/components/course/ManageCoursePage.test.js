8fb0e767b38e8865da0b2583e658c98c42ed97d3
Geert Huls <geert.huls82@gmail.com>
2016-06-12 21:11:13 +0200

    Completed test using mocks

--- src/components/course/ManageCoursePage.test.js
+++ src/components/course/ManageCoursePage.test.js
@@ -5,10 +5,21 @@ import {ManageCoursePage} from './ManageCoursePage';
 
 describe ('Manage Course Page', () => {
   it('sets error message when trying to save empty title', () => {
+    const props = {
+      authors: [],
+      //mock actions here:
+      actions: { saveCourse: () => { return Promise.resolve(); }},
+      course: {id:'', watchHref:'', title:'', authorId:'', length:'', category:''}
+    };
+
     //We need to test the component with its inner child components,
     //hence we use mount in stead of shallow.
     //A full virtual dom is created with mount (using jsdom library).
-    const wrapper = mount(<ManageCoursePage authors={[]}/>);
+    const wrapper = mount(<ManageCoursePage {...props}/>);
+    const saveButton = wrapper.find('input').last();
+    expect(saveButton.prop('type')).toBe('submit');
 
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
   });
 });
